b35ed626ea4b95a6cc56c2449fa1cab12cba9e59
hotpeperoncino <bluenemophila@gmail.com>
2017-11-15 00:57:04 -0100

    add ble_app_hrs_freertos

---
+++ app/ble_app_hrs_freertos/bspstub.c
@@ -0,0 +1,8 @@
+int bsp_board_led_invert(){}
+int bsp_board_leds_init(){}
+int bsp_board_leds_off(){}
+int bsp_indication_set(){}
+int bsp_btn_ble_sleep_mode_prepare(){}
+int bsp_btn_ble_on_ble_evt(){}
+int bsp_init(){}
+int bsp_btn_ble_init(){}
